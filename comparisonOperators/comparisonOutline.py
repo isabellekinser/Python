5322c4f9c3fc5a435614419b5acedb84e800069a
isabellekinser <93559485+isabellekinser@users.noreply.github.com>
2022-02-01 20:55:45 -0600

    2/1/22
    2/1/22

---
+++ comparisonOperators/comparisonOutline.py
@@ -0,0 +1,59 @@
+'''
+This outline will help solidify concepts from the Comparison Operators lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make two string variables. Compare them using the == operator and store
+#that comparison in a new variable. Then print the variable.
+a = apples
+b = bananas
+h = (a == b)
+print(h)
+
+
+#2) Make two int variables. Compare them using the == operator and store
+#that comparison in a new variable. Then print the variable.
+a = 12
+b = 12
+h = (a == b)
+print(h)
+
+
+#3) Make two int variables. Compare them using the > operator and store
+#that comparison in a new variable. Then print the variable.
+a = 20
+b = 12
+h = (a > b)
+print(h)
+
+
+#4) Make two int variables. Compare them using the < operator and store
+#that comparison in a new variable. Then print the variable.
+a = 12
+b = 20
+h = (a < b)
+print(h)
+
+
+#5) Make two int variables. Compare them using the >= operator and store
+#that comparison in a new variable. Then print the variable.
+a = 15
+b = 12
+h = (a >= b)
+print(h)
+
+
+#6) Make two int variables. Compare them using the <= operator and store
+#that comparison in a new variable. Then print the variable.
+a = 12
+b = 16
+h = (a <= b)
+print(h)
+
+
+#7) Make a comparison with any operator, with one of the common SYNTAX errors.
+a = 12
+b = 12
+h = (a == b)
+prnt(h)
+
---
+++ mathematicalOperators/mathOutline.py
@@ -0,0 +1,74 @@
+'''
+This outline will help solidify concepts from the Mathematical Operators lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make two string variables and use the + operator to combine them into
+#one new variable.
+x = "Volley" + "ball"
+print(x)
+
+
+#2) Make two int variables and use the + operator to combine them into
+#one new variable.
+x = 4 + 2
+print(x)
+
+
+#3) Make two int variables and use the - operator to combine them into
+#one new variable.
+x = 4 - 2 
+print(x)
+
+
+#4) Make two float variables and use the - operator to combine them into
+#one new variable.
+x = 7.2 - 1.2
+print(x)
+
+#5) Make two int variables and use the * operator to combine them into
+#one new variable.
+x = 4 * 2
+print(x)
+
+
+#6) Make one string variable and one int variable and use the * operator 
+#to combine them into one new variable.
+x = "Izzy " * 2
+print(x)
+
+
+#7) Make two int variables and use the / operator to combine them into
+#one new variable.
+x = 4 / 2
+print(x)
+
+
+#8) Make two float variables and use the / operator to combine them into
+#one new variable.
+x = 16.2 / 2
+print(x)
+
+
+#9) Make two int variables and use the % operator to combine them into
+#one new variable.
+x = 4 % 4
+print(x)
+
+
+#10) Make two int variables and use the // operator to combine them into
+#one new variable.
+x = 8 // 4
+print(x)
+
+
+#11) Make two int variables and use the ** operator to combine them into
+#one new variable.
+x = 6 ** 2
+print(x)
+
+
+#12) Make any type of operation with a common SYNTAX error
+x = 4 * 2
+prnt(x) 
+
