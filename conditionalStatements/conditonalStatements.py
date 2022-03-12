2bfc7f393243c737022ff6f05cc1909ef302243b
isabellekinser <93559485+isabellekinser@users.noreply.github.com>
2022-03-11 20:42:07 -0600

    3/11
    3/11

---
+++ conditionalStatements/conditonalStatements.py
@@ -0,0 +1,34 @@
+'''
+Created on Mar 11, 2022
+
+@author: ITAUser
+'''
+#Make a variable called score to keep track of the correct answers. And set
+#it to 0.
+score = 0
+#Ask the user question one. And store the users answer.
+#Check if the answer is correct, if it is add one to score and print out
+#"Correct"
+#Else, print out "Incorrect, the correct answer is A."
+
+#Ask the user question two. And store the users answer.
+
+#Check if the answer is correct, if it is add one to score and print out
+#"Correct"
+#Else, print out "Incorrect, the correct answer is C."
+
+#Ask the user question three. And store the users answer.
+
+#Check if the answer is correct, if it is add one to score and print out
+#"Correct"
+#Else, print out "Incorrect, the correct answer is A."
+
+#Ask the user question four. And store the users answer.
+
+#Check if the answer is correct, if it is add one to score and print out
+#"Correct"
+#Else, print out "Incorrect, the correct answer is C."
+
+#Calculate the percentage the user got. And store it in a variable called p
+
+#Print out the users score: "You got a [score]/4. Or a [p]%."
---
+++ quiz/quiz.py
@@ -0,0 +1,76 @@
+'''
+Created on Mar 11, 2022
+
+Create a working quiz on basic highschool trivia.
+
+@author: Isabelle Kinser
+'''
+#Make a variable called score to keep track of the correct answers. And set
+#it to 0.
+score=0 
+#Ask the user question one. And store the users answer.
+print("What is the powerhouse of the cell? A) mitochondria B) nucleus C) ribosome")
+
+if(answer == A):
+    print("Correct")
+    
+elif(answer == B):
+    print("Incorrect, the correct answer is A.")
+    
+elif(answer == C):
+    print ("Incorrect, the correct answer is A.")
+     
+
+#Check if the answer is correct, if it is add one to score and print out
+#"Correct"
+#Else, print out "Incorrect, the correct answer is A."
+
+#Ask the user question two. And store the users answer.
+print(" How many states comprise the United States? A) 13 B) 45 C) 50")
+
+if (answer == A):
+    print("Incorrect, the correct answer is C.")
+    
+elif(answer == B):
+    print("Incorrect, the correct answer is C.")
+    
+elif(answer == C):
+    print ("Correct")
+#Check if the answer is correct, if it is add one to score and print out
+#"Correct"
+#Else, print out "Incorrect, the correct answer is C."
+
+#Ask the user question three. And store the users answer.
+print("In y = mx + b, what does m stand for? A) slope B) output C) I don't understand math")
+
+if(answer == A):
+    print("Correct")
+    
+elif(answer == B):
+    print("Incorrect, the correct answer is A.")
+    
+elif(answer == C):
+    print ("Incorrect, the correct answer is A.")
+
+#Check if the answer is correct, if it is add one to score and print out
+#"Correct"
+#Else, print out "Incorrect, the correct answer is A."
+
+#Ask the user question four. And store the users answer.
+print("In English, a person, place or thing is called: A) verb B) adjective C) noun")"
+
+if (answer == A):
+    print("Incorrect, the correct answer is C.")
+    
+elif(answer == B):
+    print("Incorrect, the correct answer is C.")
+    
+elif(answer == C):
+    print ("Correct")
+#Check if the answer is correct, if it is add one to score and print out
+#"Correct"
+#Else, print out "Incorrect, the correct answer is C."
+
+#Calculate the percentage the user got. And store it in a variable called p
+
+#Print out the users score: "You got a [score]/4. Or a [p]%."
