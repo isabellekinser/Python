02ffa6353be544571d44cbfb21d5bc8479ef0e0b
isabellekinser <93559485+isabellekinser@users.noreply.github.com>
2022-04-06 23:25:44 -0500

    4/6
    4/6

---
+++ functions/functionOutline.py
@@ -0,0 +1,42 @@
+'''
+This outline will help solidify concepts from the Functions lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+from debugFunctionDrillsV1 import gallonsToOunces
+
+#1) Make a function that has two boolean parameters. If both booleans are
+#true, return true. Else, return false. Then, call the function.
+#Print what the function returns.
+def boolean(lessThan, greaterThan):
+    answer = <,>
+    return True
+
+x = <
+y= >
+
+boolean (x,y)
+print(boolean(x,y))
+
+#2) Make a function that takes one int parameter: gallons. Convert gallons
+#to cups (do this by multiplying gallons by 16). Then return cups. Then,
+#call the function.
+#Print what the function returns.
+def gallsonsToCups(gallons):
+    answer = gallons * 16
+    return cups
+x = gallons
+y = 16
+gallonsToCups(x,y)
+print(gallonsToCups(x,y))
+
+#3) Make a function of any any kind with a common SYNTAX error. Then, call the
+#function.
+#Print what the function returns.
+
+df gallsonsToCups(gallons)
+    answer = gallons * 16
+    return cups
+x = gallons
+y = 16
+gallonsToCups(x,y)
+print(gallonsToCups(x,y))
